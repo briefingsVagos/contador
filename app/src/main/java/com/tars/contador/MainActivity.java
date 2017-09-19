9438ecf9644100e59fd79b3d00285ef6ac041222
guibury <guilherme.bury@gmail.com>
2017-09-18 21:33:00 -0300

    Room

--- app/src/main/java/com/tars/contador/MainActivity.java
+++ app/src/main/java/com/tars/contador/MainActivity.java
@@ -2,22 +2,15 @@ package com.tars.contador;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 import com.tars.contador.contract.MVP;
 import com.tars.contador.contract.PresenterMainImpl;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 public class MainActivity extends AppCompatActivity implements MVP.MainView {
 
     private static MVP.PresenterMain presenter;
-
-    @InjectView(R.id.new_count_button)
-    Button newCountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +21,6 @@ public class MainActivity extends AppCompatActivity implements MVP.MainView {
         if (presenter == null) {
             presenter = new PresenterMainImpl();
         }
-
-        newCountButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
 }
